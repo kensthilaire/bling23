a78529101e2cd757c2cea1f789a985aa96fa65b3
EBEN YOUNG <ebenp3@gmail.com>
2017-02-11 10:59:06 -0500

    MORE FIXES for christmas

--- bling_colors.py
+++ bling_colors.py
@@ -55,10 +55,11 @@ color_map = {
     'PINKY': [ colors.Pink, colors.HotPink, colors.Salmon, colors.LightPink, colors.DeepPink, colors.Coral],
     'TEAMCOLORS' : [ colors.Blue, colors.Orange ],
     'RAINBOW': [colors.Red, colors.Orange, colors.Yellow, colors.Green, colors.Blue, colors.Purple],
-
+    'CHRISTMAS': [colors.Red, colors.Green],
+    
     # insert all color maps above this line to keep the error colors last    
     'ERROR': [colors.Red,]
-    'CHRISTMAS': [colors.Red, colors.Green]
+    
     }
 
 #
