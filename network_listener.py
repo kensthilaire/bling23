5dbdf31d3512149cb2665ae95d5fdedb3b184fe7
Ken St.Hilaire <kensthilaire@gmail.com>
2023-07-11 06:40:57 -0400

    created network tables listener example to prototype network tables integration. Will morph this into a base class that can be used for multiple things

---
+++ network_listener.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import ntcore
+import time
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
+def event_callback( event ):
+    command_str = event.data.value.getString()
+    print( 'Command Received: %s' % command_str )
+    tokens=command_str.split(',')
+    for token in tokens:
+        key,token_value=token.split('=')
+        print( '    token=%s value=%s' % (key,token_value) )
+
+if __name__ == "__main__":
+    inst = ntcore.NetworkTableInstance.getDefault()
+    inst.startClient4('Test client')
+    inst.setServerTeam(9999) # where TEAM=190, 294, etc, or use inst.setServer("hostname") or similar
+    #inst.setServer('10.99.99.2')
+    table = inst.getTable("Bling")
+
+    cmdSub = table.getStringTopic('command').subscribe('Uninitialized')
+
+    inst.addListener( cmdSub, ntcore.EventFlags.kValueAll, event_callback )
+
+    while True:
+        time.sleep(1)
+
